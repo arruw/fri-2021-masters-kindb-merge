788a1121999273484f2d1418ae2f6c6e6be3a163
Matjaž Mav <matjaz.mav@gmail.com>
2020-12-22 14:34:10 +0100

    more detailed flags

--- src/annotator-ui/main.py
+++ src/annotator-ui/main.py
@@ -22,8 +22,6 @@ class Main():
         self.annotations_file_path = annotations_file_path
         self.annotations = self._read_annotations()
 
-        self.window.protocol
-
         self.index_frame = Frame(self.master)
         self.index_frame.grid(row=0, column=0)
         self.preview_frame = Frame(self.master)
@@ -35,49 +33,73 @@ class Main():
 
         self.index_var = IntVar(value=1)
         self.index_var.trace_add("write", self.onIndexChange)
+        self.only_flagged_var = BooleanVar(value=False)
         Label(self.index_frame, text="Image ").pack(side=LEFT)
-        Entry(self.index_frame, textvariable=self.index_var,
-              justify=CENTER).pack(side=LEFT)
+        Entry(self.index_frame, textvariable=self.index_var, justify=CENTER).pack(side=LEFT)
         Label(self.index_frame, text=" of ").pack(side=LEFT)
         Label(self.index_frame, text=len(self.images)).pack(side=LEFT)
+        Checkbutton(self.index_frame, text="Only flagged", variable=self.only_flagged_var).pack(side=LEFT)
 
-        self.canvas = Canvas(self.preview_frame, width=300, height=300)
+        self.canvas = Canvas(self.preview_frame, width=512, height=512)
         self.canvas.pack(fill="both", expand=True)
 
         Button(self.buttons_frame, text="Back",
                command=self.onBackButton).pack(side=LEFT)
         Button(self.buttons_frame, text="Next",
                command=self.onNextButton).pack(side=LEFT)
+        self.window.bind('<Control-Left>', lambda _: self.onBackButton())
+        self.window.bind('<Control-Right>', lambda _: self.onNextButton())
 
         self.annotation_vars = {
             'watermark': BooleanVar(),
+            'hat': BooleanVar(),
+            'sunglasses': BooleanVar(),
+            'glasses': BooleanVar(),
+            'occlusion': BooleanVar(),
             'multiple_faces': BooleanVar(),
             'low_resolution': BooleanVar(),
+            'garbage': BooleanVar(),
             'comment': StringVar(),
         }
-        Checkbutton(self.flags_frame, text="Watermark",
-                    variable=self.annotation_vars["watermark"]).pack(side=LEFT)
-        Checkbutton(self.flags_frame, text="Multiple faces",
-                    variable=self.annotation_vars["multiple_faces"]).pack(side=LEFT)
-        Checkbutton(self.flags_frame, text="Low resolution",
-                    variable=self.annotation_vars["low_resolution"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Watermark", variable=self.annotation_vars["watermark"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Hat", variable=self.annotation_vars["hat"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Sunglasses", variable=self.annotation_vars["sunglasses"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Glasses", variable=self.annotation_vars["glasses"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Occlusion", variable=self.annotation_vars["occlusion"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Multiple faces", variable=self.annotation_vars["multiple_faces"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Low resolution", variable=self.annotation_vars["low_resolution"]).pack(side=LEFT)
+        Checkbutton(self.flags_frame, text="Garbage", variable=self.annotation_vars["garbage"]).pack(side=LEFT)
 
-        Entry(self.master, textvariable=self.annotation_vars["comment"]).grid(
-            row=4, column=0)
+        Entry(self.master, textvariable=self.annotation_vars["comment"]).grid(row=4, column=0)
 
+        self._restore_annotation()
         self._load_image()
 
     def onNextButton(self):
         self._store_annotation()
 
-        self.image_index = (self.image_index + 1) % len(self.images)
+        x = 1
+        while True: 
+            self.image_index = (self.image_index + 1) % len(self.images)
+            self._restore_annotation()
+            if not self.only_flagged_var.get() or self._is_annotation_flagged(): break
+            if x >= len(self.images): self.only_flagged_var.set(False)
+            x += 1
+
         self.index_var.set(self.image_index + 1)
         self._load_image()
 
     def onBackButton(self):
         self._store_annotation()
 
-        self.image_index = (self.image_index - 1) % len(self.images)
+        x = 1
+        while True:
+            self.image_index = (self.image_index - 1) % len(self.images)
+            self._restore_annotation()
+            if not self.only_flagged_var.get() or self._is_annotation_flagged(): break
+            if x >= len(self.images): self.only_flagged_var.set(False)
+            x += 1
+
         self.index_var.set(self.image_index + 1)
         self._load_image()
 
@@ -91,7 +113,9 @@ class Main():
                 self._store_annotation()
 
                 self.image_index = new_index
+                self._restore_annotation()
                 self._load_image()
+                
             else:
                 self.index_var.set(self.image_index + 1)
         except Exception:
@@ -101,12 +125,12 @@ class Main():
         image_path = self.images[self.image_index]
         print(f"Loading image @ {self.image_index}: {image_path}")
         self.window.title(image_path)
-        self.canvas.delete("all")
-        self.image = ImageTk.PhotoImage(Image.open(image_path))
-        self.canvas.config(width=self.image.width(),
-                           height=self.image.height())
-        self.canvas.create_image(0, 0, anchor=NW, image=self.image)
-        self._restore_annotation()
+        # self.canvas.delete("all")
+        image = Image.open(image_path)
+        image.thumbnail((512, 512), Image.ANTIALIAS)
+        self.image = ImageTk.PhotoImage(image)
+        # self.canvas.config(width=self.image.width(), height=self.image.height())
+        self.canvas.create_image(512/2-image.width/2, 512/2-image.height/2, anchor=NW, image=self.image)
 
     def _read_annotations(self):
         data = dict()
@@ -114,8 +138,7 @@ class Main():
         if not path.exists(self.annotations_file_path):
             return data
 
-        column_names = ["path", "watermark",
-                        "multiple_faces", "low_resolution", "comment"]
+        column_names = ["path", "watermark", "multiple_faces", "low_resolution", "comment", "hat", "sunglasses", "glasses", "occlusion", "garbage"]
         with open(self.annotations_file_path, 'r') as csv_file:
             reader = csv.DictReader(csv_file, fieldnames=column_names)
             next(reader)  # skip headers
@@ -125,8 +148,7 @@ class Main():
         return data
 
     def _write_annotations(self):
-        column_names = ["path", "watermark",
-                        "multiple_faces", "low_resolution", "comment"]
+        column_names = ["path", "watermark", "multiple_faces", "low_resolution", "comment", "hat", "sunglasses", "glasses", "occlusion", "garbage"]
         with open(self.annotations_file_path, "w") as csv_file:
             writer = csv.DictWriter(csv_file, fieldnames=column_names)
             writer.writeheader()
@@ -137,32 +159,42 @@ class Main():
         self.annotations[self.images[self.image_index]] = {
             'path': self.images[self.image_index],
             'watermark': self.annotation_vars["watermark"].get(),
+            'hat': self.annotation_vars["hat"].get(),
+            'sunglasses': self.annotation_vars["sunglasses"].get(),
+            'glasses': self.annotation_vars["glasses"].get(),
+            'occlusion': self.annotation_vars["occlusion"].get(),
             'multiple_faces': self.annotation_vars["multiple_faces"].get(),
             'low_resolution': self.annotation_vars["low_resolution"].get(),
+            'garbage': self.annotation_vars["garbage"].get(),
             'comment': self.annotation_vars["comment"].get()
         }
         self._write_annotations()
 
     def _restore_annotation(self):
-        annotation = self.annotations.get(
-            self.images[self.image_index], {
-                'path': self.images[self.image_index],
-                'watermark': False,
-                'multiple_faces': False,
-                'low_resolution': False,
-                'comment': ""
-            })
-        self.annotation_vars["watermark"].set(annotation["watermark"])
-        self.annotation_vars["multiple_faces"].set(
-            annotation["multiple_faces"])
-        self.annotation_vars["low_resolution"].set(
-            annotation["low_resolution"])
-        self.annotation_vars["comment"].set(annotation["comment"])
+        annotation = self.annotations.get(self.images[self.image_index], dict())
+        self.annotation_vars["watermark"].set(          self._get_or_default(annotation, "watermark", False))
+        self.annotation_vars["hat"].set(                self._get_or_default(annotation, "hat", False))
+        self.annotation_vars["sunglasses"].set(         self._get_or_default(annotation, "sunglasses", False))
+        self.annotation_vars["glasses"].set(            self._get_or_default(annotation, "glasses", False))
+        self.annotation_vars["occlusion"].set(          self._get_or_default(annotation, "occlusion", False))
+        self.annotation_vars["multiple_faces"].set(     self._get_or_default(annotation, "multiple_faces", False))
+        self.annotation_vars["low_resolution"].set(     self._get_or_default(annotation, "low_resolution", False))
+        self.annotation_vars["garbage"].set(            self._get_or_default(annotation, "garbage", False))
+        self.annotation_vars["comment"].set(            self._get_or_default(annotation, "comment", ""))
 
+    def _get_or_default(self, coll, key, default):
+        value = coll.get(key, "")
+        if value == "":
+            return default
+        return value
+
+    def _is_annotation_flagged(self):
+        return any(map(lambda var: var.get(), self.annotation_vars.values()))
 
 def main(args):
     base_path = args[1]
-    extensions = list(map(lambda ex: ex.strip(), args[2].split(",")))
+    # extensions = list(map(lambda ex: ex.strip(), args[2].split(",")))
+    extensions = ["*.jpg", "*.jpeg", "*.png", "*.JPG", "*.JPEG", "*.PNG"]
 
     print(f"Searching the following globs: ")
     images = []
--- src/fr-in-merge/main.py
+++ src/fr-in-merge/main.py
@@ -23,33 +23,41 @@ def extract_member(members_paths, relation):
         return None
 
     member_path = member
+    family_id = int(pathlib.PurePath(member).parent.name.split("_")[-1])
     member = pathlib.PurePath(member).name.upper()
     member = list(filter(lambda x: x.strip() != "", member.translate(str.maketrans(",._-;:", "      ")).split(" ")))
-    return (member_path, " ".join(member[:-1]), member[-1])
+    return (member_path, " ".join(member[:-1]), member[-1], family_id)
 
+
+def extract_members(base_path):
+    families = []
+    for family_dir in sorted(glob(f"{base_path}Family_*/")):
+        members = glob(f"{family_dir}*/")
+        father = extract_member(members, "FATHER")
+        mother = extract_member(members, "MOTHER")
+        child = extract_member(members, "CHILD")
+        if father != None and mother != None:
+            families.append((father, mother, child))
+    return families
+
+
+def save_families_to_csv(families, csv_file_path):
+    with open(csv_file_path, "w") as csv_file:
+        template = '"{0}","{1}","{2}","{3}"\n'
+        csv_file.write(template.format("Path", "Family ID", "Relation", "Name"))
+        for members in families:
+            for (path, name, relation, family_id) in list(members):
+                csv_file.write(template.format(path, family_id, relation, name))
 
 def main(argv):
     fr_base_path = "./datasets/fr/"
     in_base_path = "./datasets/in/"
 
-    fr_families = []
-    in_families = []
+    fr_families = extract_members(fr_base_path)
+    in_families = extract_members(in_base_path)
 
-    for fr_family_dir in sorted(glob(f"{fr_base_path}Family_*/")):
-        members = glob(f"{fr_family_dir}*/")
-        father = extract_member(members, "FATHER")
-        mother = extract_member(members, "MOTHER")
-        child = extract_member(members, "CHILD")
-        if father != None and mother != None:
-            fr_families.append((father, mother, child))
-
-    for in_family_dir in sorted(glob(f"{in_base_path}Family_*/")):
-        members = glob(f"{in_family_dir}*/")
-        father = extract_member(members, "FATHER")
-        mother = extract_member(members, "MOTHER")
-        child = extract_member(members, "CHILD")
-        if father != None and mother != None:
-            in_families.append((father, mother, child))
+    save_families_to_csv(fr_families, "./datasets/fr/families.csv")
+    save_families_to_csv(in_families, "./datasets/in/families.csv")
 
     # # FR & IN match
     # match_template = "|| {0:30} | {1:30} || {2:30} | {3:30} || {4:4} | {5:4} || {6:10} | {7:10} ||"
@@ -69,24 +77,24 @@ def main(argv):
     #             print(match_csv_template.format(fr_father[1], in_father[1], fr_mother[1], in_mother[1], f_match, m_match, fr_fid, in_fid))
 
 
-    # FR match
-    match_template = "|| {0:30} | {1:30} || {2:30} | {3:30} || {4:4} | {5:4} || {6:10} | {7:10} ||"
-    match_csv_template = '"{0}","{1}","{2}","{3}","{4}","{5}","{6}","{7}"'
-    print(match_csv_template.format("FR father", "IN father", "FR mother", "IN mother", "Father match %", "Mother match %", "FR family ID", "IN family ID"))
-    for (fr_father, fr_mother, _) in fr_families:
-        for (in_father, in_mother, _) in fr_families:
-            fr_fid = pathlib.PurePath(fr_father[0]).parent.name
-            in_fid = pathlib.PurePath(in_father[0]).parent.name
-            if fr_fid == in_fid: continue
+    # # FR match
+    # match_template = "|| {0:30} | {1:30} || {2:30} | {3:30} || {4:4} | {5:4} || {6:10} | {7:10} ||"
+    # match_csv_template = '"{0}","{1}","{2}","{3}","{4}","{5}","{6}","{7}"'
+    # print(match_csv_template.format("FR father", "IN father", "FR mother", "IN mother", "Father match %", "Mother match %", "FR family ID", "IN family ID"))
+    # for (fr_father, fr_mother, _) in fr_families:
+    #     for (in_father, in_mother, _) in fr_families:
+    #         fr_fid = pathlib.PurePath(fr_father[0]).parent.name
+    #         in_fid = pathlib.PurePath(in_father[0]).parent.name
+    #         if fr_fid == in_fid: continue
 
-            fr_name_tokens = fr_father[1] + " " + fr_mother[1]
-            in_name_tokens = in_father[1] + " " + in_mother[1]
-            f_match = fuzz.token_sort_ratio(fr_father[1], in_father[1])
-            m_match = fuzz.token_sort_ratio(fr_mother[1], in_mother[1])
+    #         fr_name_tokens = fr_father[1] + " " + fr_mother[1]
+    #         in_name_tokens = in_father[1] + " " + in_mother[1]
+    #         f_match = fuzz.token_sort_ratio(fr_father[1], in_father[1])
+    #         m_match = fuzz.token_sort_ratio(fr_mother[1], in_mother[1])
 
 
-            if (f_match >= 75 or m_match >= 75) and  f_match + m_match >= 100:
-                print(match_csv_template.format(fr_father[1], in_father[1], fr_mother[1], in_mother[1], f_match, m_match, fr_fid, in_fid))
+    #         if (f_match >= 75 or m_match >= 75) and  f_match + m_match >= 100:
+    #             print(match_csv_template.format(fr_father[1], in_father[1], fr_mother[1], in_mother[1], f_match, m_match, fr_fid, in_fid))
 
 
 
